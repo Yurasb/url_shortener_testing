6023eb0d682a860a763fd0a98f7e7b7ddb902628
Yury Kuptsou <yurasb@tut.by>
2018-01-02 01:02:59 +0300

    1. added postive_stats_redirected test case 2. clean up

--- test_cases/test_case.py
+++ test_cases/test_case.py
@@ -20,12 +20,24 @@ positive_stats_new = TestCase(
     method='POST',
     data=u'{"id": "a6b6d734-8e73-4179-aa8b-8a7994eaa691"}',
     exp_scheme={
-        'last_redirected': {'nullable': True, 'type': 'datetime'},
-        'redirects_count': {'type': 'integer', 'max': 0}
+        'last_redirected': {'nullable': True, 'type': 'float'},
+        'redirects_count': {'type': 'integer', 'min': 0, 'max': 0}
     },
     exp_status_code=200,
     exp_headers={'Content-Length': '47', 'Content-Type': 'application/json; charset=utf-8'},
     exp_encoding='utf-8'
 )
 
-TEST_SUITE = [positive_shortcut, positive_stats_new]
+positive_stats_redirected = TestCase(
+    method='POST',
+    data='{"id": "f256ee21-ff33-4f8e-84f4-c947496ed227"}',
+    exp_scheme={
+        'last_redirected': { 'type': 'float'},
+        'redirects_count': {'type': 'integer', 'min': 1, 'max': 1}
+    },
+    exp_status_code=200,
+    exp_headers= {'Content-Length': '60', 'Content-Type': 'application/json; charset=utf-8'},
+    exp_encoding='utf-8'
+)
+
+TEST_SUITE = [positive_shortcut, positive_stats_new, positive_stats_redirected]
