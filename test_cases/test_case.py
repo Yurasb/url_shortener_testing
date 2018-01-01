3d7583b70a403b7b080bdbb690e342fcc176a2ba
Yury Kuptsou <yurasb@tut.by>
2018-01-01 21:09:28 +0300

    Fixed scheme for positive_stats_new test case - 'redirect counts' 0 only allowed

--- test_cases/test_case.py
+++ test_cases/test_case.py
@@ -19,7 +19,10 @@ positive_shortcut = TestCase(
 positive_stats_new = TestCase(
     method='POST',
     data=u'{"id": "a6b6d734-8e73-4179-aa8b-8a7994eaa691"}',
-    exp_scheme={'last_redirected': {'nullable': True, 'type': 'datetime'}, 'redirects_count': {'type': 'integer'}},
+    exp_scheme={
+        'last_redirected': {'nullable': True, 'type': 'datetime'},
+        'redirects_count': {'type': 'integer', 'max': 0}
+    },
     exp_status_code=200,
     exp_headers={'Content-Length': '47', 'Content-Type': 'application/json; charset=utf-8'},
     exp_encoding='utf-8'
