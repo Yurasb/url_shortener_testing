f88ef72b0cb21f7a6b89dd2a9338d89d9c8a0d58
Yury Kuptsou <yurasb@tut.by>
2018-07-03 13:59:52 +0300

    refactoring by comments from review

--- tests/test_shortcut.py
+++ tests/test_shortcut.py
@@ -8,17 +8,14 @@ from network.client import create_shortcut_by_http, create_shortcut_by_ws
 
 @allure.feature('Shortcut handler')
 @allure.story('Valid HTTP request')
-def test_http_shortcut_valid_request(http_client, test_context, expected_schema):
-    client = http_client
-    actual_response = client.create_shortcut(test_context)
-    assert_that(actual_response.status_code, equal_to(200), 'status code')
-    assert_that(actual_response.json(), match_to(expected_schema), 'response body')
+def test_valid_http_request():
+    actual_response = create_shortcut_by_http(payloads.VALID_SHORTCUT_PAYLOAD)
+    assert_that(actual_response, match_to(schemas.VALID_SHORTCUT_RESPONSE))
 
 
 @allure.feature('Shortcut handler')
 @allure.story('Valid WebSocket request')
-def test_ws_shortcut_valid_request(ws_client, ws_test_context, expected_schema):
-    client = ws_client
-    actual_response = client.create_shortcut(ws_test_context)
+def test_valid_ws_request():
+    actual_response = create_shortcut_by_ws(payload=payloads.VALID_SHORTCUT_PAYLOAD)
     assert_that(actual_response['code'], equal_to(200), 'status code')
     assert_that(actual_response['body'], match_to(schemas.VALID_SHORTCUT_RESPONSE), 'response body')
