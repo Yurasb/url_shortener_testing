88a7f16b9138ef8fe98e9f4435b231b50c547dfe
Yury Kuptsou <yurasb@tut.by>
2018-07-04 20:34:54 +0300

    Refactoring

--- tests/test_all_links.py
+++ tests/test_all_links.py
@@ -4,19 +4,33 @@ import allure
 from hamcrest import assert_that
 
 from client import http, ws
-from data import schemas
+from data import schemas, payloads
 from tests.matchers import match_to
 
 
 @allure.feature('All links handler')
-@allure.story('HTTP Valid payload when DB is empty')
+@allure.story('HTTP GET when DB is empty')
 def test_http_valid_payload_when_db_is_empty():
     response = http.get_all_links()
     assert_that(response, match_to(schemas.VALID_ALL_LINKS_EMPTY_LIST_RESPONSE))
 
 
 @allure.feature('All links handler')
-@allure.story('WebSocket request when DB is empty')
+@allure.story('WebSocket GET when DB is empty')
 def test_ws_valid_payload_when_db_is_empty():
     response = ws.get_all_links()
     assert_that(response, match_to(schemas.VALID_ALL_LINKS_EMPTY_LIST_RESPONSE))
+
+
+@allure.feature('All links handler')
+@allure.story('HTTP DELETE with Confirmation')
+def test_http_valid_payload(create_shortcut_link):
+    response = http.purge_all_links(payloads.VALID_PURGE_PAYLOAD)
+    assert_that(response, match_to(schemas.VALID_PURGE_RESPONSE))
+
+
+@allure.feature('All links handler')
+@allure.story('WebSocket DELETE with Confirmation')
+def test_ws_valid_payload(create_shortcut_link):
+    response = ws.purge_all_links(payloads.VALID_PURGE_PAYLOAD)
+    assert_that(response, match_to(schemas.VALID_PURGE_RESPONSE))
